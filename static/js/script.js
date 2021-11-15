b7587c69c0bafd9cee307f7eb488709e2ab52cb6
skywalkerdll <584547953@qq.com>
2021-11-15 21:53:19 +0800

    mod js

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,6 @@
 document.getElementById("hover").addEventListener("mouseenter", function () {
   document.getElementById("draw").classList.add("animate");
-  
+  document.getElementById("draw").click();
   let audio = document.getElementById("music");
   if (audio.paused){
      audio.play();

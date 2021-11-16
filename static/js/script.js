b8fa8c165b4e5fa4d696a0c03e6c29515cc64b65
skywalkerdll <584547953@qq.com>
2021-11-16 22:22:45 +0800

    1

--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,30 @@
-document.getElementById("hover").addEventListener("mouseenter", function () {
-  document.getElementById("draw").classList.add("animate");
-  setInterval(playmusic(),1000); 
-
-});
-
-document.getElementById("hover").addEventListener("mouseleave", function () {
-  document.getElementById("draw").classList.remove("animate");
-});
-
-// var svgdocment=document.getElementById('svgobject').contentDocument;
-// var svgdocment = document.getElementById('svgembed').getSVGDocument();
-
-// svgdocment.getElementById("hover").addEventListener("mouseenter", function () {
-//   svgdocment.getElementById("draw").classList.add("animate");
+// document.getElementById("hover").addEventListener("mouseenter", function () {
+//   document.getElementById("draw").classList.add("animate");
 //   setInterval(playmusic(),1000); 
 
 // });
 
-// svgdocment.getElementById("hover").addEventListener("mouseleave", function () {
-//   svgdocment.getElementById("draw").classList.remove("animate");
+// document.getElementById("hover").addEventListener("mouseleave", function () {
+//   document.getElementById("draw").classList.remove("animate");
 // });
+
+window.onload = function () {
+
+  var a=document.getElementById('svgObject');
+  var svgdocment=a.contentDocument;
+  //var svgdocment = document.getElementById('svgembed').getSVGDocument();
+
+  svgdocment.getElementById("hover").addEventListener("mouseenter", function () {
+    document.getElementById("draw").classList.add("animate");
+    setInterval(playmusic(),1000); 
+
+  });
+
+  svgdocment.getElementById("hover").addEventListener("mouseleave", function () {
+    document.getElementById("draw").classList.remove("animate");
+  });
+
+}
 
 function playmusic(){
   let audio = document.getElementById("music");

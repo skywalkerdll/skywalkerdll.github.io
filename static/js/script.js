c304bca78b5346c1707e995fc5701f4cf88ad88b
skywalkerdll <584547953@qq.com>
2021-11-16 22:37:11 +0800

    4

--- static/js/script.js
+++ static/js/script.js
@@ -1,30 +1,30 @@
-// document.getElementById("hover").addEventListener("mouseenter", function () {
-//   document.getElementById("draw").classList.add("animate");
-//   setInterval(playmusic(),1000); 
+document.getElementById("hover").addEventListener("mouseenter", function () {
+  document.getElementById("draw").classList.add("animate");
+  setInterval(playmusic(),1000); 
 
-// });
+});
 
-// document.getElementById("hover").addEventListener("mouseleave", function () {
-//   document.getElementById("draw").classList.remove("animate");
-// });
+document.getElementById("hover").addEventListener("mouseleave", function () {
+  document.getElementById("draw").classList.remove("animate");
+});
 
-window.onload = function () {
+// window.onload = function () {
 
-  var a=document.getElementById('svgObject');
-  var svgdocment=a.contentDocument();
-  //var svgdocment = document.getElementById('svgembed').getSVGDocument();
+//   var a=document.getElementById('svgObject');
+//   var svgdocment=a.contentDocument();
+//   //var svgdocment = document.getElementById('svgembed').getSVGDocument();
 
-  svgdocment.getElementById("hover").addEventListener("mouseenter", function () {
-    document.getElementById("draw").classList.add("animate");
-    setInterval(playmusic(),1000); 
+//   svgdocment.getElementById("hover").addEventListener("mouseenter", function () {
+//     document.getElementById("draw").classList.add("animate");
+//     setInterval(playmusic(),1000); 
 
-  });
+//   });
 
-  svgdocment.getElementById("hover").addEventListener("mouseleave", function () {
-    document.getElementById("draw").classList.remove("animate");
-  });
+//   svgdocment.getElementById("hover").addEventListener("mouseleave", function () {
+//     document.getElementById("draw").classList.remove("animate");
+//   });
 
-}
+// }
 
 function playmusic(){
   let audio = document.getElementById("music");

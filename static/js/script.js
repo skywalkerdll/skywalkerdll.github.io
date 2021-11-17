7689ed671daff6ed5621aa228f2f44dbbc6dd1f2
skywalkerdll <584547953@qq.com>
2021-11-17 22:53:02 +0800

    2

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,9 @@
-<<<<<<< HEAD
-=======
-document.getElementById("hover").addEventListener("mouseenter", function () {
-  document.getElementById("draw").classList.add("animate");
-  setInterval(playmusic(),1000); 
-});
-
-document.getElementById("hover").addEventListener("mouseleave", function () {
-  document.getElementById("draw").classList.remove("animate");
-});
  
->>>>>>> 83e55dca729a4cc651f47887a913b356db63deba
-
 function playmusic(){
   let audio = document.getElementById("music");
   if (audio.paused){
      audio.play();
   }
-<<<<<<< HEAD
 }
 
 window.onload = function () {
@@ -31,6 +18,3 @@ window.onload = function () {
   });
 
 }
-=======
-}
->>>>>>> 83e55dca729a4cc651f47887a913b356db63deba

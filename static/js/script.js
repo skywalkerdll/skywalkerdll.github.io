2c7a01f1b589a44578f61bbca23cc17b76776d3e
skywalkerdll <584547953@qq.com>
2021-11-17 13:36:21 +0800

    1

---
+++ static/js/script.js
@@ -0,0 +1,16 @@
+document.getElementById("hover").addEventListener("mouseenter", function () {
+  document.getElementById("draw").classList.add("animate");
+  setInterval(playmusic(),1000); 
+});
+
+document.getElementById("hover").addEventListener("mouseleave", function () {
+  document.getElementById("draw").classList.remove("animate");
+});
+ 
+
+function playmusic(){
+  let audio = document.getElementById("music");
+  if (audio.paused){
+     audio.play();
+  }
+}
